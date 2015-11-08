696eb76a02f7ba334e287543def2c38e049c7c8e
Erick Rivas <rivadunga@hotmail.com>
2015-11-07 19:33:42 -0600

    Start laser implementation

--- Space fall/Assets/Scripts/InputData.cs
+++ Space fall/Assets/Scripts/InputData.cs
@@ -7,6 +7,8 @@ public class InputData : MonoBehaviour {
 	public static bool move_down = false;
 	public static bool move_left = false;
 	public static bool move_right = false;
+	public static bool shot_pressed = false;
+	public static bool shot_down = false;
 	public static bool cam_up = false;
 	public static bool cam_down = false;
 	public static bool cam_left = false;
@@ -19,13 +21,15 @@ public class InputData : MonoBehaviour {
 
 	private void updateControls()
 	{
-		cam_up    = Input.GetKey (KeyCode.UpArrow);
-		cam_down  = Input.GetKey (KeyCode.DownArrow);
-		cam_left  = Input.GetKey (KeyCode.LeftArrow);
-		cam_right = Input.GetKey (KeyCode.RightArrow);
 		move_up    = Input.GetKey (KeyCode.W);
 		move_down  = Input.GetKey (KeyCode.S);
 		move_left  = Input.GetKey (KeyCode.A);
 		move_right = Input.GetKey (KeyCode.D);
+		shot_down = Input.GetKeyDown (KeyCode.Space);
+		shot_pressed  = Input.GetKey (KeyCode.Space);
+		cam_up    = Input.GetKey (KeyCode.UpArrow);
+		cam_down  = Input.GetKey (KeyCode.DownArrow);
+		cam_left  = Input.GetKey (KeyCode.LeftArrow);
+		cam_right = Input.GetKey (KeyCode.RightArrow);
 	}
 }
